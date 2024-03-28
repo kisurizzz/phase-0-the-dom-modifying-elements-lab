5b6c5b5aefc0171d7c8df233ca9cabc7c2681bc3
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-03-28 06:41:47 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainChild = document.querySelector('main#main')
+mainChild.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Arnold Kisuri'
+newHeader.textContent = `${yourName} is the champion`
